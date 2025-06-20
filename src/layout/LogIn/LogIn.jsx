8cb2c772debe3629d689791355599c1aa7c9070d
Salohiddin231 <davletovsalohiddin4@gmail.com>
2025-06-20 12:06:42 +0600

    login

--- src/layout/LogIn/LogIn.jsx
+++ src/layout/LogIn/LogIn.jsx
@@ -1,8 +1,35 @@
+import AdminLogo from '../../assets/icons/AdminLogo.svg'
+
 export default function LogIn() {
     return (
-        <>
-            <div className="loginBg">
+        <div className="loginContainer">
+            <div className="loginBg"></div>
+            <div className="loginCenter">
+                <form className="login">
+                    <img src={AdminLogo} alt="" />
+                    <h1>Добро пожаловать</h1>
+                    <div className="loginwrap1">
+                        <label htmlFor="phone">Введите номер телефона</label>
+                        <input
+                            type="number"
+                            placeholder="+ 996 "
+                            style={{
+                                MozAppearance: 'textfield',
+                                WebkitAppearance: 'none',
+                                appearance: 'none'
+                            }}
+                        />
+                    </div>
+                    <div className="loginwrap2">
+                        <label htmlFor="password">Пароль</label>
+                        <input
+                            type="password"
+                            placeholder="Введите пароль"
+                        />
+                    </div>
+                    <button className="loginBtn">Войти</button>
+                </form>
             </div>
-        </>
+        </div>
     )
 }
